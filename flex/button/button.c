04c51247f57894ac8c1e4ed900309a5097071499
Leon Anavi <leon@anavi.org>
2016-07-20 02:54:26 +0300

    button: Application to monitor the button
    Simple C application which prints text when
    the button on RabbitMax Flex is released after it
    has been pressed for at least a second.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ flex/button/button.c
@@ -0,0 +1,34 @@
+#include <wiringPi.h>
+#include <stdio.h>
+
+//Pin 23 on Raspberry Pi corresponds to BCM GPIO 11 and wiringPi pin 14
+#define ButtonPin 14
+
+int main(void)
+{
+  if(-1 == wiringPiSetup())
+  {
+      printf("setup wiringPi failed!");
+      return 1;
+  }
+
+  //Set GPIO pin
+  pinMode(ButtonPin, INPUT);
+  pullUpDnControl(ButtonPin, PUD_UP);
+
+  //Monitor the button and print text if button
+  //has been released after at least a second
+  int buf = HIGH;
+  while(1)
+  {
+    int status = digitalRead(ButtonPin);
+    if ( (HIGH == status) && (LOW == buf) )
+    {
+	printf("Button released!\n");	
+    }
+    buf = status;
+    delay(1000);
+  }
+
+  return 0;
+}
