f95336aa6d3daa555354ae89d9b557d0db30958a
Leon Anavi <leon@anavi.org>
2016-08-17 03:11:55 +0300

    button.c: Add instructions
    Add instructions to explain how the example works
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- flex/button/button.c
+++ flex/button/button.c
@@ -6,29 +6,31 @@
 
 int main(void)
 {
-  if(-1 == wiringPiSetup())
-  {
-      printf("setup wiringPi failed!");
-      return 1;
-  }
+	if(-1 == wiringPiSetup())
+	{
+			printf("setup wiringPi failed!");
+			return 1;
+	}
 
-  //Set GPIO pin
-  pinMode(ButtonPin, INPUT);
-  pullUpDnControl(ButtonPin, PUD_UP);
+	//Set GPIO pin
+	pinMode(ButtonPin, INPUT);
+	pullUpDnControl(ButtonPin, PUD_UP);
 
-  //Monitor the button and print text if button
-  //has been released after at least a second
-  int buf = HIGH;
-  while(1)
-  {
-    int status = digitalRead(ButtonPin);
-    if ( (HIGH == status) && (LOW == buf) )
-    {
-	printf("Button released!\n");	
-    }
-    buf = status;
-    delay(1000);
-  }
+	printf("Press the button and release it after at least a second.\n");
 
-  return 0;
+	//Monitor the button and print text if button
+	//has been released after at least a second
+	int buf = HIGH;
+	while(1)
+	{
+		int status = digitalRead(ButtonPin);
+		if ( (HIGH == status) && (LOW == buf) )
+		{
+			printf("Button released!\n");
+		}
+		buf = status;
+		delay(1000);
+	}
+
+	return 0;
 }
