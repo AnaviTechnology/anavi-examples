429c4c0672c0aed8231aee0ceb928feb9115b128
Leon Anavi <leon@anavi.org>
2016-09-20 23:56:52 +0300

    relay.py: Python example for the relay
    Add simple Python script to turn on and off the
    relay and move the exaple written in C to another
    directory.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ flex/relay/python/relay.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+#Relay - pin 29 on Raspberry Pi corresponds to BCM GPIO 5
+RelayPin = 29
+
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+# set up GPIO output channel
+GPIO.setup(RelayPin, GPIO.OUT)
+
+GPIO.output(RelayPin, GPIO.HIGH)
+time.sleep(3)
+GPIO.output(RelayPin, GPIO.LOW)
+GPIO.cleanup() 
