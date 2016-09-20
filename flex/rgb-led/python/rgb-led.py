8b4e899435fe1fdb744b8cb2293b5e4106cbc6f0
Leon Anavi <leon@anavi.org>
2016-09-20 23:29:18 +0300

    rgb-led.py: Python example for RGB LED
    Create simple Python application and move the
    sample application written in C to a separate
    directory.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ flex/rgb-led/python/rgb-led.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as GPIO
+import time
+import signal
+import sys
+
+#Blue - pin 33 on Raspberry Pi corresponds to BCM GPIO 13
+Pin0 = 33
+#Green - pin 35 on Raspberry Pi corresponds to BCM GPIO 19
+Pin1 = 35
+#Red - pin 37 on Raspberry Pi corresponds to BCM GPIO 26
+Pin2 = 37
+
+def led(red, green, blue):
+	GPIO.output(Pin0, blue)
+        GPIO.output(Pin1, green)
+        GPIO.output(Pin2, red)
+	return
+
+def close(signal, frame):
+        print("\nTurning off lights...\n")
+	led(GPIO.LOW, GPIO.LOW, GPIO.LOW)
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, close)
+
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+# set up GPIO output channel
+GPIO.setup(Pin0, GPIO.OUT)
+GPIO.setup(Pin1, GPIO.OUT)
+GPIO.setup(Pin2, GPIO.OUT)
+
+#reset
+led(GPIO.LOW, GPIO.LOW, GPIO.LOW)
+
+while True:
+        led(GPIO.HIGH, GPIO.LOW, GPIO.LOW)
+	time.sleep(3)
+	led(GPIO.LOW, GPIO.HIGH, GPIO.LOW)
+	time.sleep(3)
+	led(GPIO.LOW, GPIO.LOW, GPIO.HIGH)
+	time.sleep(3)
+GPIO.cleanup() 
