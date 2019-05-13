416d3531a7d6bd70389351c091dc21c7d8524870
Leon Anavi <leon@anavi.org>
2019-05-13 08:41:02 +0300

    oled-dispay-sketch.ino: Fix tabs
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- peripherals/oled-dispay-sketch/oled-dispay-sketch.ino
+++ peripherals/oled-dispay-sketch/oled-dispay-sketch.ino
@@ -11,40 +11,40 @@ const int pinButton = 0;
 
 void drawDisplay(const char *line1, const char *line2 = "", const char *line3 = "")
 {
-    // Write on OLED display
-    // Clear the internal memory
-    u8g2.clearBuffer();
-    // Set appropriate font
-    u8g2.setFont(u8g2_font_ncenR14_tr);
-    u8g2.drawStr(0,14, line1);
-    u8g2.drawStr(0,39, line2);
-    u8g2.drawStr(0,64, line3);
-    // Transfer internal memory to the display
-    u8g2.sendBuffer();
+  // Write on OLED display
+  // Clear the internal memory
+  u8g2.clearBuffer();
+  // Set appropriate font
+  u8g2.setFont(u8g2_font_ncenR14_tr);
+  u8g2.drawStr(0,14, line1);
+  u8g2.drawStr(0,39, line2);
+  u8g2.drawStr(0,64, line3);
+  // Transfer internal memory to the display
+  u8g2.sendBuffer();
 }
 
 void setup()
 {
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    Serial.println();
-    u8g2.begin();
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println();
+  u8g2.begin();
 
-    delay(10);
+  delay(10);
 
-    //Button
-    pinMode(pinButton, INPUT);
+  //Button
+  pinMode(pinButton, INPUT);
 
-    Serial.println("Testing the OLED I2C display...");
-    drawDisplay("TEST", "Hello", "World");
+  Serial.println("Testing the OLED I2C display...");
+  drawDisplay("TEST", "Hello", "World");
 }
 
 void loop()
 {
-   if (false == digitalRead(pinButton))
-   {
-       Serial.println("Rebooting...");
-       // Restart the board
-       ESP.restart(); 
-   }
+  if (false == digitalRead(pinButton))
+  {
+    Serial.println("Rebooting...");
+    // Restart the board
+    ESP.restart(); 
+  }
 }
