67b39e3f48c963d9572b6b660808270371e1767e
Leon Anavi <leon@anavi.org>
2019-07-27 22:59:06 +0300

    oled-dispay-sketch.ino: Check I2C OLED display
    Add a quick check at the beginning to verify if mini OLED
    display has been attached by testing I2C address 3c. Keep
    in mind that this is the default address of the supported
    OLED display. Some other display models may have a
    different I2C address.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- peripherals/oled-dispay-sketch/oled-dispay-sketch.ino
+++ peripherals/oled-dispay-sketch/oled-dispay-sketch.ino
@@ -4,10 +4,13 @@
 
 // For OLED display
 #include <U8g2lib.h>
+// For I2C
+#include <Wire.h>
 
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 const int pinButton = 0;
+const int i2cDisplayAddress = 0x3c;
 
 void drawDisplay(const char *line1, const char *line2 = "", const char *line3 = "")
 {
@@ -23,11 +26,36 @@ void drawDisplay(const char *line1, const char *line2 = "", const char *line3 = 
   u8g2.sendBuffer();
 }
 
+bool isSensorAvailable(int sensorAddress)
+{
+  // Check if I2C sensor is present
+  Wire.beginTransmission(sensorAddress);
+  return 0 == Wire.endTransmission();
+}
+
+void checkDisplay()
+{
+  Serial.print("Mini I2C OLED Display at address ");
+  Serial.print(i2cDisplayAddress, HEX);
+  if (isSensorAvailable(i2cDisplayAddress))
+  {
+      Serial.println(": OK");
+  }
+  else
+  {
+      Serial.println(": N/A");
+  }
+}
+
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println();
+
+  Wire.begin();
+  checkDisplay();
+
   u8g2.begin();
 
   delay(10);
