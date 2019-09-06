3e3094b6a82bdc66d243adb394177449afbacac1
Leon Anavi <leon@anavi.org>
2019-09-06 14:05:00 +0300

    diy-breathalyzer.ino: Example for MQ-3
    Example for ANAVI Gas Detector and MQ-3 analog sensor module.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ anavi-gas-detector/diy-breathalyzer/diy-breathalyzer.ino
@@ -0,0 +1,115 @@
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+// For OLED display
+#include <U8g2lib.h>
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+// Configure pins
+const int pinAlarm = 16;
+const int pinButton = 0;
+const int pinCustom = 2;
+const int pinGreen = 13;
+const int pinYellow = 12;
+const int pinRed = 14;
+const int pinAdc = 0;
+
+unsigned long gasPreviousMillis = 0;
+// Read data from the analog MQ-3 sensor on every second
+const long gasInterval = 1000;
+
+void drawDisplay(const char *line1, const char *line2 = "", const char *line3 = "")
+{
+    // Write on OLED display
+    // Clear the internal memory
+    u8g2.clearBuffer();
+    // Set appropriate font
+    u8g2.setFont(u8g2_font_ncenR14_tr);
+    u8g2.drawStr(0,14, line1);
+    u8g2.drawStr(0,39, line2);
+    u8g2.drawStr(0,64, line3);
+    // Transfer internal memory to the display
+    u8g2.sendBuffer();
+}
+
+void setColor(bool red, bool yellow, bool green)
+{
+  digitalWrite(pinRed, red);
+  digitalWrite(pinYellow, yellow);
+  digitalWrite(pinGreen, green);
+}
+
+void detectAlcohol()
+{
+  int breath = analogRead(pinAdc);
+  // Calculate conductivity in pecents
+  // The alcohol concetration in the air depends on the coductivity
+  // If the analog MQ-3 sensor detects more alcohol gases
+  // the conductivity will be higher
+  int conductivity = round(((float)breath/1023)*100);
+
+  String state = "Sober";
+  if (breath <= 200)
+  {
+    setColor(LOW, LOW, HIGH);
+  }
+  else if (breath <= 350)
+  {
+    state="on 1-2 beers";
+    setColor(LOW, HIGH, LOW);
+  }
+  else if (breath <= 500)
+  {
+    state="Drunk!";
+    setColor(HIGH, LOW, LOW);
+  }
+  else
+  {
+    state="Totally wasted!";
+    setColor(HIGH, LOW, LOW);
+  }
+
+  String line2 = "You are: ";
+  String line3 = state ;
+
+  // Print values in the serial output
+  Serial.print("Alcohol value: ");
+  Serial.println(breath);
+  Serial.print("Conductivity: ");
+  Serial.print(conductivity);
+  Serial.println("%");
+  Serial.print(line2);
+   Serial.println(line3);
+
+  drawDisplay("Breathanalyzer", line2.c_str(), line3.c_str());
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println();
+    
+    u8g2.begin();
+    delay(10);
+
+    //LEDs
+    pinMode(pinAlarm, OUTPUT);
+    pinMode(pinRed, OUTPUT);
+    pinMode(pinYellow, OUTPUT);
+    pinMode(pinGreen, OUTPUT);
+    //Button
+    pinMode(pinButton, INPUT);
+    //Custom GPIO
+    pinMode(pinCustom, OUTPUT);
+}
+
+void loop()
+{
+    const unsigned long currentMillis = millis();
+    // Update the values from the analog MQ gas sensor
+    if (gasInterval <= (currentMillis - gasPreviousMillis))
+    {
+        gasPreviousMillis = currentMillis;
+        detectAlcohol();
+    }
+}
