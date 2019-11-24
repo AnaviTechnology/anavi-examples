b2e43ce9019117afc166f55ace758b18c15ad0ef
Leon Anavi <leon@anavi.org>
2019-11-25 01:04:18 +0200

    anavi-miracle-controller-gestures.ino: Example
    Add an example for turning on and off consecutively LED on
    addressable LED strips using ANAVI Miracle Controller and
    I2C gesture sensor module APD9960.
    
    When gesture to the left is detected turn off the LED, when
    gesture to the right is detected turn on the next LED.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ anavi-miracle-controller/anavi-miracle-controller-gestures/anavi-miracle-controller-gestures.ino
@@ -0,0 +1,99 @@
+#include <FS.h> 
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#include "Adafruit_APDS9960.h"
+
+// For FastLED
+#define FASTLED_ESP8266_RAW_PIN_ORDER
+#include <FastLED.h>
+
+#define LED_PIN1    12
+#define LED_PIN2    14
+#define NUM_LEDS    15
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2812
+#define COLOR_ORDER GRB
+#define FRAMES_PER_SECOND  120
+
+Adafruit_APDS9960 apds;
+
+CRGB leds1[NUM_LEDS];
+CRGB leds2[NUM_LEDS];
+
+int currentPosition = 1;
+
+void updateColor(int numToFill)
+{
+  // Turn off all LEDs
+  fill_solid(leds1, NUM_LEDS, CRGB::Black);
+  // Turn on LEDs depending the gesture
+  fill_rainbow(leds1, numToFill, 0, 7);
+
+  // Turn off all LEDs
+  fill_solid(leds2, NUM_LEDS, CRGB::Black);
+  // Turn on LEDs depending the gesture
+  fill_rainbow(leds2, numToFill, 0, 7);
+
+  
+  FastLED.show();
+  FastLED.delay(1000/FRAMES_PER_SECOND);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  Serial.println("ANAVI Miracle Controller");
+  Serial.println("------------------------------");
+  Serial.println("Control LED strip with gesture");
+  Serial.println("------------------------------");
+
+  FastLED.addLeds<LED_TYPE, LED_PIN1, COLOR_ORDER>(leds1, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<LED_TYPE, LED_PIN2, COLOR_ORDER>(leds2, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+  updateColor(currentPosition);
+  delay(100);
+  Serial.println("LED strips initialized.");
+
+  if(!apds.begin())
+  {
+    Serial.println("ERROR: Failed to initialize gesture sensor! Please check your wiring.");
+  }
+  else Serial.println("Gesture sensor initialized.");
+
+  //gesture mode will be entered once proximity mode senses something close
+  apds.enableProximity(true);
+  apds.enableGesture(true);
+}
+
+void loop()
+{
+  //read a gesture from the device
+  uint8_t gesture = apds.readGesture();
+  if(APDS9960_LEFT == gesture)
+  {
+    if (1 < currentPosition)
+    {
+      Serial.println("Go left!");
+      currentPosition -= 1;
+      updateColor(currentPosition);
+    }
+    else
+    {
+      Serial.println("You have already reached the maximum on the left.");
+    }
+  }
+  else if(APDS9960_RIGHT == gesture)
+  {
+    if (15 > currentPosition)
+    {
+      Serial.println("Go right!");
+      currentPosition += 1;
+      updateColor(currentPosition);
+    }
+    else
+    {
+      Serial.println("You have already reached the maximum on the right.");
+    }
+  }
+}
