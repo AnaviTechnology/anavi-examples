3978d2701eef04dde83d52794ec363d43494e9d9
Leon Anavi <leon@anavi.org>
2021-02-22 01:50:36 +0200

    anavi-led-dimmer: Add pir-motion-led-dimmer.ino
    Add Arduino example sketch for detecting motion with PIR sensor.
    Turn on the lights for 10 seconds if motion is detected. It is
    also possible to turn on or off the lights manually by pressing
    the button.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ anavi-led-dimmer/pir-motion-led-dimmer/pir-motion-led-dimmer.ino
@@ -0,0 +1,80 @@
+// Pin for the button
+#define BUT 1
+// Pin to drive the LED strip
+#define PWM 0
+// Analog 1 corresponds to pin PB2
+#define POT 1
+#define PIR 4
+// Time period to keep the lights on
+#define DELAY 10000
+
+int potValue = 0;
+
+// the current state of LED
+int ledState = HIGH;
+// the previous state of button
+int lastButtonState = HIGH;
+// the current state of button
+int currentButtonState = HIGH;
+
+int pirState = LOW;
+unsigned long motionTime = 0;
+
+void setup()
+{
+	pinMode(PWM, OUTPUT);
+	analogWrite(PWM, 0);
+	pinMode(BUT, INPUT_PULLUP);
+	currentButtonState = digitalRead(BUT);
+	pinMode(PIR, INPUT_PULLUP);
+}
+
+void loop()
+{
+	// Allow to turn on or off the lights with the button
+	// no matter what is the state of the PIR motion sensor
+	lastButtonState = currentButtonState;
+	// Check the status of the button
+	currentButtonState = digitalRead(BUT);
+	if ( (HIGH == lastButtonState) && (LOW == currentButtonState) )
+	{
+		// toggle state of LED
+		ledState = !ledState;
+		if (LOW != ledState)
+		{
+			motionTime = millis();
+		}
+	}
+
+	if (HIGH == digitalRead(PIR))
+	{
+		if (LOW == pirState)
+		{
+			motionTime = millis();
+			ledState = HIGH;
+			pirState = HIGH;
+		}
+	}
+	else
+	{
+		pirState = LOW;
+	}
+
+	if ((millis() - DELAY) > motionTime)
+	{
+		ledState = LOW;
+	}
+	
+	if (LOW == ledState)
+	{
+		// Turn off the LED strip
+		analogWrite(PWM, 0);
+	}
+	else
+	{
+		// Read potentiometer value in range 0 - 1024 
+		// and map it to the LED
+		potValue = map(analogRead(1), 0, 1023, 0, 255);
+		analogWrite(PWM, potValue);
+	}
+}
