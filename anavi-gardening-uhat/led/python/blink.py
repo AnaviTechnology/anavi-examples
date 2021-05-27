ad488b15e23a3e3d9b7782e04915c9f04fdac191
Leon Anavi <leon@anavi.org>
2021-05-28 01:39:20 +0300

    anavi-gardening-uhat/led/python/blink.py: 2 LEDs
    Support a couple of LEDs, aka LED1 and LED2.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- anavi-gardening-uhat/led/python/blink.py
+++ anavi-gardening-uhat/led/python/blink.py
@@ -5,12 +5,15 @@ import time
 
 import RPi.GPIO as GPIO
 
-# Pin 15 on Raspberry Pi corresponds to GPIO 3
-LED = 15
+# Pin 15 on Raspberry Pi corresponds to GPIO 22
+LED1 = 15
+# Pin 16 on Raspberry Pi corresponds to GPIO 23
+LED2 = 16
 
 
 def close(signal, frame):
-    GPIO.output(LED, 0)
+    GPIO.output(LED1, 0)
+    GPIO.output(LED2, 0)
     sys.exit(0)
 
 
@@ -21,15 +24,18 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 # set up GPIO output channel
-GPIO.setup(LED, GPIO.OUT)
+GPIO.setup(LED1, GPIO.OUT)
+GPIO.setup(LED2, GPIO.OUT)
 
 # reset
 
 try:
     while True:
-        GPIO.output(LED, 1)
+        GPIO.output(LED1, 1)
+        GPIO.output(LED2, 0)
         time.sleep(0.5)
-        GPIO.output(LED, 0)
+        GPIO.output(LED1, 0)
+        GPIO.output(LED2, 1)
         time.sleep(0.5)
 finally:
     GPIO.cleanup()
