677a314d7cf2edf143cefc893e081a91116468e6
Leon Anavi <leon@anavi.org>
2022-05-15 23:39:48 +0300

    anavi-gardening-uhat/led/c: Use pigpio
    Use library pigpio for controlling LED D1 and D2 on
    ANAVI Gardening uHAT.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- anavi-gardening-uhat/led/c/blink.c
+++ anavi-gardening-uhat/led/c/blink.c
@@ -1,39 +1,50 @@
-#include <wiringPi.h>
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <pigpio.h>
 
-//Pin 15 on Raspberry Pi corresponds to BCM GPIO 3 and wiringPi pin 3
-#define LED 3
+//Pin 15 on Raspberry Pi corresponds to BCM GPIO 22
+#define D1 22
+//Pin 16 on Raspberry Pi corresponds to BCM GPIO 23
+#define D2 23
 
-void close()
+void terminate()
 {
-	digitalWrite(LED, 0);
+	gpioWrite(D1, 0);
+	gpioWrite(D2, 0);
+
+	//Stop DMA, release resources
+	gpioTerminate();
 	exit(0);
 }
 
 int main()
 {
-	signal(SIGINT, close);
-
-	if(-1 == wiringPiSetup())
+	if (0 > gpioInitialise())
 	{
-		printf("setup wiringPi failed!");
+		fprintf(stderr, "pigpio initialisation failed\n");
 		return 1;
 	}
 
-	//Set GPIO pins
-	pinMode(LED, OUTPUT);
+	// Set GPIOs to output modes
+	gpioSetMode(D1, PI_OUTPUT);
+	gpioSetMode(D2, PI_OUTPUT);
+
+	signal(SIGINT, terminate);
 
 	// Blink
 	while(1)
 	{
-		digitalWrite(LED, 1);
-		delay(500);
-		digitalWrite(LED, 0);
-		delay(500);
+		gpioWrite(D1, 1);
+		gpioWrite(D2, 0);
+		time_sleep(0.5);
+		gpioWrite(D1, 0);
+		gpioWrite(D2, 1);
+		time_sleep(0.5);
 	}
+
+	// Stop DMA, release resources
+	gpioTerminate();
 
 	return 0;
 }
-
