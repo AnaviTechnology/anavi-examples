8fcf6446fd110ab8fd1b116a99ed5ef427ea1864
Leon Anavi <leon@anavi.org>
2023-04-20 18:38:51 +0300

    anavi-miracle-uhat: Add NeoPixels example
    Add a simple Python script to demonstrate NeoPixels, aka
    addressable RGB LEDs. The script is based on:
    https://learn.adafruit.com/neopixels-on-raspberry-pi
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ anavi-miracle-uhat/neopixels/rainbow.py
@@ -0,0 +1,70 @@
+# SPDX-FileCopyrightText: 2023 Leon Anavi
+# SPDX-License-Identifier: MIT
+
+# Based on https://learn.adafruit.com/neopixels-on-raspberry-pi
+
+import time
+import signal
+import sys
+import board
+import neopixel
+
+
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+
+# The number of NeoPixels
+num_pixels = 10
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+def close(signal, frame):
+    print("\nGoodbye!")
+    pixels.fill((0, 0, 0))
+    pixels.show()
+    sys.exit(0)
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+signal.signal(signal.SIGINT, close)
+
+print("ANAVI Miracle uHAT")
+
+while True:
+    # rainbow cycle with 50ms delay per step
+    rainbow_cycle(0.05)
