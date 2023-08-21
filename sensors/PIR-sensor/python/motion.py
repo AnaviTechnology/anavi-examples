c2ecfddc47c3d438cb0d542f5afd2d7a2a3bb1bf
Desislava Angelova <angelova.desi.g@gmail.com>
2023-08-21 17:04:55 +0300

    PIR/python/motion.py: Ctrl+C and error handling
    Support Ctrl+C and error handling.
    
    Signed-off-by: Desislava Angelova <angelova.desi.g@gmail.com>

--- sensors/PIR-sensor/python/motion.py
+++ sensors/PIR-sensor/python/motion.py
@@ -1,18 +1,32 @@
-from gpiozero import MotionSensor
+import os
+import sys
+import signal
 from signal import pause
-pir = MotionSensor(4)
-motion_count = 0
+from gpiozero import MotionSensor
 
 def motion_function():
-    global motion_count
-    motion_count += 1
-    print(f'Motion Detected: {motion_count}')
+  global motion_count
+  motion_count += 1
+  print(f'Motion Detected: {motion_count}')
 
 def no_motion_function():
-    print(f'Motion stopped: {motion_count}')
+  print(f'Motion stopped: {motion_count}')
 
-pir.when_motion = motion_function
-pir.when_no_motion = no_motion_function
+def signal_handler(sig, frame):
+  print("\nApplication termiated with Ctrl+C.")
+  os._exit(0)
 
-pause()
+try:
+  signal.signal(signal.SIGINT, signal_handler)
 
+  pir = MotionSensor(4)
+  motion_count = 0
+
+  pir.when_motion = motion_function
+  pir.when_no_motion = no_motion_function
+
+  pause()
+
+except:
+  print("Unexpected error:", sys.exc_info()[0])
+  os._exit(2)
