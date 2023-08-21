c9284447ea76c41c9e1354d4e190e6c6a57462e3
Desislava Angelova <angelova.desi.g@gmail.com>
2023-08-21 16:55:40 +0300

    APDS-9960/python/gesture.py: Add error handling
    Add error handling and support to terminate the application with
    Ctrl + C from the command-line interface.
    
    Signed-off-by: Desislava Angelova <angelova.desi.g@gmail.com>

--- sensors/APDS-9960/python/gesture.py
+++ sensors/APDS-9960/python/gesture.py
@@ -1,36 +1,53 @@
+import os
+import sys
+import signal
 from apds9960.const import *
 from apds9960 import APDS9960
 import RPi.GPIO as GPIO
 import smbus
 from time import sleep
 
-port = 1
-bus = smbus.SMBus(port)
-
-apds = APDS9960(bus)
+def signal_handler(sig, frame):
+  print("\nApplication termiated with Ctrl+C.")
+  os._exit(0)
 
 dirs = {
-    APDS9960_DIR_NONE: "none",
-    APDS9960_DIR_LEFT: "left",
-    APDS9960_DIR_RIGHT: "right",
-    APDS9960_DIR_UP: "up",
-    APDS9960_DIR_DOWN: "down",
-    APDS9960_DIR_NEAR: "near",
-    APDS9960_DIR_FAR: "far",
+  APDS9960_DIR_NONE: "none",
+  APDS9960_DIR_LEFT: "left",
+  APDS9960_DIR_RIGHT: "right",
+  APDS9960_DIR_UP: "up",
+  APDS9960_DIR_DOWN: "down",
+  APDS9960_DIR_NEAR: "near",
+  APDS9960_DIR_FAR: "far",
 }
 try:
-    apds.setProximityIntLowThreshold(50)
+  signal.signal(signal.SIGINT, signal_handler)
 
-    print("APDS9960 Gesture Test")
-    print("============")
-    apds.enableGestureSensor()
-    while True:
-        sleep(0.5)
-        if apds.isGestureAvailable():
-            motion = apds.readGesture()
-            print("Gesture={}".format(dirs.get(motion, "unknown")))
+  port = 1
+  bus = smbus.SMBus(port)
+  apds = APDS9960(bus)
 
+  apds.setProximityIntLowThreshold(50)
+
+  print("APDS9960 Gesture Test")
+  print("============")
+  apds.enableGestureSensor()
+
+  while True:
+    sleep(0.5)
+
+    if apds.isGestureAvailable():
+      motion = apds.readGesture()
+      print("Gesture={}".format(dirs.get(motion, "unknown")))
+
+except OSError:
+  print('No sensor found.')
+  os._exit(1)
+
+except:
+  print("Unexpected error:", sys.exc_info()[0])
+  os._exit(2)
 
 finally:
-    GPIO.cleanup()
-    print("Bye")
+  GPIO.cleanup()
+  print("Bye")
