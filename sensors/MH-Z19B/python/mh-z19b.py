a79844713ec88f07464eca84bf7bc60fb02aee69
Desislava Angelova <angelova.desi.g@gmail.com>
2023-08-21 17:26:07 +0300

    MH-Z19B/python/mh-z19b.py: Error handling
    Add error handling.
    
    Signed-off-by: Desislava Angelova <angelova.desi.g@gmail.com>

--- sensors/MH-Z19B/python/mh-z19b.py
+++ sensors/MH-Z19B/python/mh-z19b.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 
 import mh_z19
+import serial
 import sys
 
-mh_z19.detection_range_10000()
-data = mh_z19.read_all()
-print('CO2: ' + str(data["co2"]) + 'ppm')
-print('Temperature: ' + str(data["temperature"]) + 'C')
-sys.exit(0)
+try:
+  mh_z19.detection_range_10000()
+  data = mh_z19.read_all()
+  print('CO2: ' + str(data["co2"]) + 'ppm')
+  print('Temperature: ' + str(data["temperature"]) + 'C')
+except serial.serialutil.SerialException as error:
+  print(error)
+  print("Have you tried running it with sudo?")
+except KeyError:
+  print("Unable to read sensor data.")
+  print("Please check the wiring and wait for the preheat time before using the sensor.")
+else:
+  sys.exit(0)
